4fd39ab882ea32c30a7e9cfa1c74414ae560faca
Justin Borzi <justinborzi@gmail.com>
2022-09-12 17:08:09 -0400

    Fix a small concatenation issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
 #include <Arduino.h>
 
+/* 
+* I, Justin Borzi, 000798465 certify that this material is my original
+* work. No other person's work has been used without due 
+* acknowledgement. (Replace with your own name and student number)
+*/
+
 void setup() {
-  // put your setup code here, to run once:
+  // Setup monitor thread.
   Serial.begin(115200);
-  
-  Serial.println("\n\nHello World!\n\n");
+
+  // Print Hello world. (1/2)
+  Serial.println("\n\nHello World!");
+
+  // Print Who I am, and the device's information.
   Serial.println("\nJustin Borzi, 000798465");
-  Serial.println("Chip ESP8266 Chip ID: " + ESP.getChipId());
-  Serial.println("Flash Chip ID: " + ESP.getFlashChipId());
+  Serial.print("Chip ESP8266 Chip ID: ");
+  Serial.println(ESP.getChipId());
+  Serial.print("Flash Chip ID: ");
+  Serial.println(ESP.getFlashChipId());
+  Serial.println();
 }
 
 void loop() {
-  // Delay the execution for 2 seconds, then print the total elapsed miliseconds.
+  // Delay the execution for 2 seconds then print the time elapsed
+  // since the board was started
   delay(2000);
   Serial.println(millis());
 }
